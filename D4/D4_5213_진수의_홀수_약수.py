decbc3fe712aabe7f8870c2f054b40b9be3ea8af
SaHong Pak <k3y6reak@naver.com>
2019-02-26 15:10:47 +0900

    Update D4_5213_진수의_홀수_약수.py

--- D4/D4_5213_진수의_홀수_약수.py
+++ D4/D4_5213_진수의_홀수_약수.py
@@ -1,14 +1,14 @@
 nums = [0]*1000001
 
 def main():
-    for i in range(1, 1000001, 2):
+    for i in range(1, 1000001, 2): # i는 홀수만 가져온다.
         j = i
-        while j <= 1000000:
-            nums[j] += i
+        while j <= 1000000: # 해당 홀수부터~
+            nums[j] += i # 1000000 까지 각 수들의 홀수 합을 저장.
             j += i
 
     for i in range(1, 1000000):
-        nums[i + 1] += nums[i]
+        nums[i + 1] += nums[i] # 이전까지의 합을 더해준다.
 
     tc = int(input())
     for t in range(1, tc+1):
