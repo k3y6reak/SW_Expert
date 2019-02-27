111d34286160e2c9ab453400b989220ae7be9792
SaHong Pak <k3y6reak@naver.com>
2019-02-27 09:43:24 +0900

    [+] Edit

--- Tech/fibo_피보나치_메모.py
+++ Tech/fibo_피보나치_메모.py
@@ -1,9 +1,9 @@
 fibo_list = [0]*10000
 
 
-def fibo(n, fibo_list):
+def fibo(n):
     if n >= 2 and fibo_list[n] == 0:
-        fibo_list[n] = fibo(n-1, fibo_list) + fibo(n-2, fibo_list)
+        fibo_list[n] = fibo(n-1) + fibo(n-2)
 
     return fibo_list[n]
 
@@ -12,7 +12,7 @@ def main():
     n = 3
     fibo_list[0] = 1
     fibo_list[1] = 1
-    print(fibo(n, fibo_list))
+    print(fibo(n))
 
 
 if __name__ == '__main__':
